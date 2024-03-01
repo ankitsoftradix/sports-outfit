76a781a46de1cb1800dba40bb95d97415ea24850
Ankit <ankit@softradix.com>
2024-03-01 18:29:15 +0530

    shirt color changed

--- src/components/Model/index.jsx
+++ src/components/Model/index.jsx
@@ -1,8 +1,32 @@
 import { useGLTF } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense, useEffect, useMemo, useRef } from "react";
+import { Color } from "three";
+import { useBearStore } from "../../store";
 
 const Model = (props) => {
+  const tieRef = useRef();
+  const shirtRef = useRef();
+  const palletsRef = useRef();
   const { nodes, materials } = useGLTF("./models/t-shirt.glb");
+
+  const tieMaterial = useMemo(() => {
+    return materials.material_0.clone();
+  }, [materials]);
+  const shirtMaterial = useMemo(() => {
+    return materials.material_0.clone();
+  }, [materials]);
+
+  const colorPickerList = useBearStore((state) => state.colorPickerList);
+
+  useEffect(() => {
+    const tieColor = colorPickerList[0].color;
+    const shirtColor = colorPickerList[1].color;
+    const palletsColor = colorPickerList[2].color;
+    tieRef.current.material.color = new Color(tieColor);
+    shirtRef.current.material.color = new Color(shirtColor);
+    palletsRef.current.material.color = new Color(palletsColor);
+  }, [colorPickerList]);
+
   return (
     <Suspense>
       <ambientLight intensity={3} />
@@ -10,21 +34,26 @@ const Model = (props) => {
       <group {...props} dispose={null} position={[0, -4, 0]} scale={0.7}>
         <group rotation={[-Math.PI / 2, 0, 0]}>
           <mesh
+            ref={palletsRef}
             geometry={nodes.Object_2.geometry}
             material={materials.material_0}
           />
           <mesh
+            ref={shirtRef}
             geometry={nodes.Object_3.geometry}
-            material={materials.material_0}
+            material={shirtMaterial}
           />
           <mesh
+            ref={tieRef}
             geometry={nodes.Object_4.geometry}
-            material={materials.material_0}
+            material={tieMaterial}
           />
         </group>
       </group>
     </Suspense>
   );
 };
+
+useGLTF.preload("./models/t-shirt.glb");
 
 export default Model;
--- src/components/Tabs/Color/index.jsx
+++ src/components/Tabs/Color/index.jsx
@@ -1,9 +1,21 @@
+import { useBearStore } from "../../../store";
 import styles from "./Color.module.scss";
+import Picker from "./Picker";
 
 const Color = () => {
+  const colorPickerList = useBearStore((state) => state.colorPickerList);
+
   return (
     <div className={styles.main}>
       <div className={styles.heading}>Choose colors</div>
+      {colorPickerList.map((item) => (
+        <Picker
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          color={item.color}
+        />
+      ))}
     </div>
   );
 };
